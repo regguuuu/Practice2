c662cfb7024477d7e4a8775762dcf39e5989ea1f
regguuuu <Reggie.Leon@cdpipelinedevshop.com>
2022-08-26 19:22:17 -0400

    first commit

---
+++ src/ProblemSet2_1.java
@@ -0,0 +1,17 @@
+public class ProblemSet2_1 {
+    public static void main(String[] args) {
+        //Use 8 print statements to print a smiley face.
+        //Your art will rely on only a single character, besides space, such as X or #.
+        System.out.println("  WAKE  ");
+        System.out.println(" U    P ");
+        System.out.println("L x  x L");
+        System.out.println("L      L");
+        System.out.println("L L  L L");
+        System.out.println("L  LL  L");
+        System.out.println(" L    L ");
+        System.out.println("  LLLL  ");
+        
+        
+        
+    }   
+}
